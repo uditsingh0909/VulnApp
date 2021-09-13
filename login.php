3cad330b710d8f66b1d4639bb8437c41635b351c
Chandan Singh <chandancodes@gmail.com>
2021-09-13 21:53:19 +0530

    login ui fix

--- login.php
+++ login.php
@@ -111,21 +111,37 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
+<style>
+.circles{position:absolute;top:0;left:0;width:100%;height:100%;overflow:hidden;}.circles li{position:absolute;display:block;list-style:none;width:20px;height:20px;background:#cc0000;animation:animate 25s linear infinite;bottom:-150px}.circles li:nth-child(1){left:25%;width:80px;height:80px;animation-delay:0s;}.circles li:nth-child(2){left:10%;width:20px;height:20px;animation-delay:2s;animation-duration:2s;}.circles li:nth-child(3){left:70%;width:20px;height:20px;animation-delay:4s;}.circles li:nth-child(4){left:40%;width:60px;height:60px;animation-delay:0s;animation-duration:18s;}.circles li:nth-child(5){left:65%;width:20px;height:20px;animation-delay:0s;}.circles li:nth-child(6){left:75%;width:110px;height:110px;animation-delay:3s;}.circles li:nth-child(7){left:35%;width:150px;height:150px;animation-delay:7s;}.circles li:nth-child(8){left:50%;width:25px;height:25px;animation-delay:15s;animation-duration:45s;}.circles li:nth-child(9){left:20%;width:15px;height:15px;animation-delay:2s;animation-duration:35s;}.circles li:nth-child(10){left:85%;width:150px;height:150px;animation-delay:0s;animation-duration:11s;}@keyframes animate{0%{transform:translateY(0) rotate(0deg);opacity:1;border-radius:0}100%{transform:translateY(-1000px) rotate(720deg);opacity:1;border-radius:50%}}
+</style>
 </head>
-
-<body class="bg-gradient-primary">
+<body class="bg-gradient-primary" style="background-color: #000;">
+<div>
+    <ul class="circles">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+</div>
   <div class="container">
     <!-- Outer Row -->
     <div class="row justify-content-center">
-      <div class="col-xl-10 col-lg-12 col-md-9">
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
+    <div class="col-md-5">
+        <div class="card o-hidden border-0 shadow-lg my-5" style="background-color: #131417;">
+          <div class="card-body p-0" style="padding-right: -19px;margin-right: -90px;">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6">
+              <div class="col-md-10">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <h1 class="h4 text-gray-900 mb-4"><b>Sign In</b></h1>
                   </div>
                   <form action="" method="post" class="user">
                     <div class="form-group">
@@ -141,7 +157,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
                           Me</label>
                       </div>
                     </div>
-                    <button type="submit" class="btn btn-primary btn-user btn-block">Submit</button>
+                    <button type="submit" class="btn btn-primary btn-user btn-block">Sign In</button>
                   </form>
                 </div>
               </div>
