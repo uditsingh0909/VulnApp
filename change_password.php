7083fedca491b77f1817d8ace73aba56bee691aa
udit singh <jarves.pro@gmail.com>
2021-09-22 23:50:57 +0530

    v0.5

--- change_password.php
+++ change_password.php
@@ -431,7 +431,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
             </div>
         </div>
     </div>
-
+    <?php include('footer.php');?>
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
